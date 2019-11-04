e728e0f2db1f8930b809e9c5e7ecf8000a48a639
BartlomiejDarocha <bartlomiejdarocha@gmail.com>
2019-11-04 21:35:53 +0100

    kolejna sposob na header

--- trainProject/src/app/intercereptor/intercerepotr.ts
+++ trainProject/src/app/intercereptor/intercerepotr.ts
@@ -9,18 +9,27 @@ export class Interceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log('intercerpot works');
         const token: string = localStorage.getItem('token');
+        const test = {
+            headers: new HttpHeaders(),
+        };
+        test.headers = test.headers.append('X-accces', '1');
 
+        console.log(test.headers, 'test Headers');
         // let headers2: HttpHeaders = new HttpHeaders();
         // headers2 = headers2.append('Authorization', 'testToke');
         // headers2 = headers2.append('Content-Type', 'application/json');
-        if (token) { // login np;
-            req = req.clone({ headers: req.headers.set('Authorization', 'testToke' + token) });
-        }
-        if (!req.headers.has('Content-Type')) {// sprawdznie cotentu
-            req = req.clone({ headers: req.headers.set('Content-Type', 'application/json') });
-        }
-        req = req.clone({ headers: req.headers.set('Accept', 'application/json') });
-        //console.log(headers2, 'headers2');
+
+        // to jest istotne dzialajacy interceptor 
+        // if (token) { // login np;
+        //     req = req.clone({ headers: req.headers.set('Authorization', 'testToke' + token) });
+        // }
+        // if (!req.headers.has('Content-Type')) {// sprawdznie cotentu
+        //     req = req.clone({ headers: req.headers.set('Content-Type', 'application/json') });
+        // }
+        // req = req.clone({ headers: req.headers.set('Accept', 'application/json') });
+
+        // to jest TEST  innej metody dodawanie Hedarsow 
+        req = req.clone({headers: test.headers});
         return next.handle(req).pipe(
             map((event: HttpEvent<any>) => {
                 if (event instanceof HttpResponse) {
